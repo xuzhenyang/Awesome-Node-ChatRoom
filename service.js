c6aa53b0fae6bbdeaddc214ff12dbcb9cf11ece1
Xuzy <xuzywozz@163.com>
2016-06-22 11:58:50 +0800

    整合前端

--- service.js
+++ service.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
 
 var onlineUsers = {};
 var onlineCount = 0;
@@ -20,10 +23,11 @@ io.on('connection', function (socket) {
 
     socket.on('login', function (loginInfo) {
         console.log("username : " + loginInfo.username);
+        console.log("userid : " + loginInfo.userid);
         //socket的name作为标识 用于logout
-        socket.name = loginInfo.username;
-        if (!onlineUsers.hasOwnProperty(loginInfo.username)) {
-            onlineUsers[loginInfo.username] = loginInfo.username;
+        socket.name = loginInfo.userid;
+        if (!onlineUsers.hasOwnProperty(loginInfo.userid)) {
+            onlineUsers[loginInfo.userid] = loginInfo.userid;
             onlineCount++;
         }
         io.emit('loginInfo', { onlineUsers: onlineUsers, onlineCount: onlineCount });
@@ -37,13 +41,13 @@ io.on('connection', function (socket) {
             roomList[loginInfo.room] = new Room(loginInfo.room);
         }
         //判断房间内的用户对象是否存在 若不存在则创建用户对象并统计人数
-        if (!(roomList[loginInfo.room].users).hasOwnProperty(loginInfo.username)) {
-            roomList[loginInfo.room].users[loginInfo.username] = loginInfo.username;
+        if (!(roomList[loginInfo.room].users).hasOwnProperty(loginInfo.userid)) {
+            roomList[loginInfo.room].users[loginInfo.userid] = loginInfo.userid;
             roomList[loginInfo.room].count++;
         }
         socket.join(loginInfo.room);
         io.to(loginInfo.room).emit('joinInfo', roomList[loginInfo.room]);
-        io.to(loginInfo.room).emit('sysInfo','----' + loginInfo.username + " 加入 " + loginInfo.room + '----');
+        io.to(loginInfo.room).emit('sysInfo', '----' + loginInfo.username + " 加入 " + loginInfo.room + '----');
     });
 
     socket.on('chat message', function (msg) {
@@ -54,16 +58,25 @@ io.on('connection', function (socket) {
     socket.on('leave', function (leaveInfo) {
         console.log('user leave');
         // console.log('room ' + leaveInfo.room + ' username ' + leaveInfo.username);
-        if((roomList[leaveInfo.room].users).hasOwnProperty(socket.name))
-        {
+        if ((roomList[leaveInfo.room].users).hasOwnProperty(socket.name)) {
             delete (roomList[leaveInfo.room].users)[socket.name];
             roomList[leaveInfo.room].count--;
         }
         io.to(leaveInfo.room).emit('leaveInfo', roomList[leaveInfo.room]);
-        io.to(leaveInfo.room).emit('sysInfo','----' + leaveInfo.username + " 退出 " + leaveInfo.room + '----');
+        io.to(leaveInfo.room).emit('sysInfo', '----' + leaveInfo.username + " 退出 " + leaveInfo.room + '----');
     });
 
     socket.on('disconnect', function () {
+        for (room in roomList) {
+            // console.log(roomList[room].users);
+            if ((roomList[room].users).hasOwnProperty(socket.name)) {
+                delete (roomList[room].users)[socket.name];
+                roomList[room].count--;
+            }
+            io.to(roomList[room].roomname).emit('leaveInfo', roomList[room]);
+            io.to(roomList[room].roomname).emit('sysInfo', '----' + socket.name + " 退出 " + roomList[room].roomname + '----');
+        }
+
         console.log('user disconnected');
         if (onlineUsers.hasOwnProperty(socket.name)) {
             delete onlineUsers[socket.name];
