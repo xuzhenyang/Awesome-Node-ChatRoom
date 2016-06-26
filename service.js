a3693beaa1f7b9f178996ac2822aad12cb701198
Xuzy <xuzywozz@163.com>
2016-06-26 13:59:11 +0800

    merge from allmelgr

--- service.js
+++ service.js
@@ -98,3 +98,5 @@ io.on('connection', function (socket) {
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
+
+// browser-sync start --proxy "localhost:3000" --files "**/*.css, **/*.html"
