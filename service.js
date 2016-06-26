6fc5793e9ed0e6107e2452489c3a0c93e7d9051a
Xuzy <xuzywozz@163.com>
2016-06-26 15:02:58 +0800

    remobe onlineUsers object and fix bug(cause of  forget to do 'socket.leave')

--- service.js
+++ service.js
@@ -9,14 +9,28 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-var onlineUsers = {};
-var onlineCount = 0;
 var roomList = {};
 var Room = function (roomname) {
     this.roomname = roomname;
     this.users = {};
     this.count = 0;
 };
+var User = function (userid, username) {
+    this.userid = userid;
+    this.username = username;
+}
+
+function showAllRoom() {
+    console.log('---showAllRoom---');
+    // console.log(roomList);
+    for (room in roomList) {
+        console.log('--' + roomList[room].roomname + '--');
+        for (user in roomList[room].users) {
+            console.log(roomList[room].users[user].userid + ' : ' + roomList[room].users[user].username);
+        }
+    }
+    console.log('---showAllRoom---');
+}
 
 io.on('connection', function (socket) {
     console.log("a user connected");
@@ -28,22 +42,19 @@ io.on('connection', function (socket) {
         socket.name = loginInfo.userid;
         //socket的name2作为标识，用于登出时显示用户名
         socket.name2 = loginInfo.username;
-        if (!onlineUsers.hasOwnProperty(loginInfo.userid)) {
-            onlineUsers[loginInfo.userid] = loginInfo.userid;
-            onlineCount++;
-        }
-        io.emit('loginInfo', { onlineUsers: onlineUsers, onlineCount: onlineCount });
+        io.emit('loginInfo', 'login success...');
     });
 
     socket.on('join', function (joinInfo) {
-        console.log(joinInfo.username + "join in : " + joinInfo.room);
+        console.log('socketname ' + socket.name + ' ：' + joinInfo.username + " 想要加入 " + joinInfo.room);
         //判断房间对象是否存在 若不存在则创建
         if (!roomList.hasOwnProperty(joinInfo.room)) {
             roomList[joinInfo.room] = new Room(joinInfo.room);
         }
         //判断房间内的用户对象是否存在 若不存在则创建用户对象并统计人数
         if (!(roomList[joinInfo.room].users).hasOwnProperty(joinInfo.userid)) {
-            roomList[joinInfo.room].users[joinInfo.userid] = joinInfo.userid;
+            console.log('----add user----');
+            roomList[joinInfo.room].users[joinInfo.userid] = new User(joinInfo.userid, joinInfo.username);
             roomList[joinInfo.room].count++;
         }
         //把下面一段放进上面的if里...其他房间的人也能知道该用户加入了哪个房间...
@@ -63,18 +74,18 @@ io.on('connection', function (socket) {
         // console.log('room ' + leaveInfo.room + ' username ' + leaveInfo.username);
         //判断房间内的用户对象是否存在
         if ((roomList[leaveInfo.room].users).hasOwnProperty(socket.name)) {
+            console.log('----delete user----');
             delete (roomList[leaveInfo.room].users)[socket.name];
             roomList[leaveInfo.room].count--;
-
         }
-        //把下面一段放进上面的if里...其他房间的人也能知道该用户退出了哪个房间...
-        //另外多次加入退出后好像也会出现该bug
+        socket.leave(leaveInfo.room);
         io.to(leaveInfo.room).emit('leaveInfo', roomList[leaveInfo.room]);
         io.to(leaveInfo.room).emit('sysInfo', '----' + leaveInfo.username + " 退出 " + leaveInfo.room + '----');
 
     });
 
     socket.on('disconnect', function () {
+        console.log('user disconnected');
         for (room in roomList) {
             // console.log(roomList[room].users);
             if ((roomList[room].users).hasOwnProperty(socket.name)) {
@@ -84,14 +95,7 @@ io.on('connection', function (socket) {
             io.to(roomList[room].roomname).emit('leaveInfo', roomList[room]);
             io.to(roomList[room].roomname).emit('sysInfo', '----' + socket.name2 + " 退出 " + roomList[room].roomname + '----');
         }
-
-        console.log('user disconnected');
-        if (onlineUsers.hasOwnProperty(socket.name)) {
-            delete onlineUsers[socket.name];
-            onlineCount--;
-        }
-        io.emit('logoutInfo', { onlineUsers: onlineUsers, onlineCount: onlineCount });
-        // io.emit('sysInfo','----' + socket.name + '已退出聊天' + '----');
+        io.emit('logoutInfo', 'logout success...');
     });
 });
 
